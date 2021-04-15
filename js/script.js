b54f9d8a92ecfd26b3ef7b77ef8c6deed7dffc79
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-15 12:48:32 +0300

    Added modulus funtion to perform modulus and return a result.

--- js/script.js
+++ js/script.js
@@ -5,18 +5,27 @@
 //   var number2 = parseInt(prompt("Enter the second number"));
 //   alert(add(number1, number2));
 
-  var subtract = function(number1, number2) {
-    return number1 - number2;
+  // var subtract = function(number1, number2) {
+  //   return number1 - number2;
+  // };
+  //  var num1 = parseInt(prompt("Enter number1:")); 
+  //  var num2 = parseInt(prompt("Enter num2"));
+  //  alert(subtract(num1 , num2));
+
+
+  // var multiply = function(number1, number2) {
+  //   return number1 * number2;
+  // };
+  //  var num1 = parseInt(prompt("Enter number1:")); 
+  //  var num2 = parseInt(prompt("Enter num2"));
+  //  alert(multiply(num1 , num2));
+
+
+  var modulus = function(number1, number2) {
+    return number1 % number2;
   };
    var num1 = parseInt(prompt("Enter number1:")); 
    var num2 = parseInt(prompt("Enter num2"));
-   alert(subtract(num1 , num2));
+   alert(modulus(num1 , num2));
 
-
-  var multiply = function(number1, number2) {
-    return number1 * number2;
-  };
-   var num1 = parseInt(prompt("Enter number1:")); 
-   var num2 = parseInt(prompt("Enter num2"));
-   alert(multiply(num1 , num2));
 
