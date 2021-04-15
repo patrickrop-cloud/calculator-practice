e00bf5683372d1b218ed55f956546a68a1f45d89
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-15 12:39:19 +0300

    Added subtration funtion to subtract two input numbers and return the result.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-var add = function(number1, number2) {
-    return number1 + number2;
+// var add = function(number1, number2) {
+//     return number1 + number2;
+//   };
+//   var number1 = parseInt(prompt("Enter the first number:"));
+//   var number2 = parseInt(prompt("Enter the second number"));
+//   alert(add(number1, number2));
+
+  var subtract = function(number1, number2) {
+    return number1 - number2;
   };
-  var number1 = parseInt(prompt("Enter the first number:"));
-  var number2 = parseInt(prompt("Enter the second number"));
-  alert(add(number1, number2));
-  
+   var num1 = parseInt(prompt("Enter number1:")); 
+   var num2 = parseInt(prompt("Enter num2"));
+   alert(subtract(num1 , num2));
+
