a10f6e417054f4a043121d45638c1e0f1a0492a8
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-15 13:25:12 +0300

    Added division function to js and added

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var add = function(number1, number2) {
   var subtract = function(number1, number2) {
     return number1 - number2;
   };
-   var num1 = parseInt(prompt("Enter number1:")); 
+   var num1 = parseInt(prompt("Enter num1:")); 
    var num2 = parseInt(prompt("Enter num2"));
    alert(subtract(num1 , num2));
 
@@ -16,7 +16,7 @@ var add = function(number1, number2) {
   var multiply = function(number1, number2) {
     return number1 * number2;
   };
-   var num1 = parseInt(prompt("Enter number1:")); 
+   var num1 = parseInt(prompt("Enter num1:")); 
    var num2 = parseInt(prompt("Enter num2"));
    alert(multiply(num1 , num2));
 
@@ -24,8 +24,18 @@ var add = function(number1, number2) {
   var modulus = function(number1, number2) {
     return number1 % number2;
   };
-   var num1 = parseInt(prompt("Enter number1:")); 
+   var num1 = parseInt(prompt("Enter num1:")); 
    var num2 = parseInt(prompt("Enter num2"));
    alert(modulus(num1 , num2));
+
+
+  var division = function(number1, number2) {
+    return number1 / number2;
+  };
+   var num1 = parseInt(prompt("Enter num1:")); 
+   var num2 = parseInt(prompt("Enter num2"));
+   alert(division(num1 , num2));
+   
+
 
 
