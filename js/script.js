7531a26bf0a2d5c2640467f1d8369ba2d479b3b4
Patrick Rop <patrick.rop@student.moringaschool.com>
2021-04-15 13:05:09 +0300

    Added a css styling

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-// var add = function(number1, number2) {
-//     return number1 + number2;
-//   };
-//   var number1 = parseInt(prompt("Enter the first number:"));
-//   var number2 = parseInt(prompt("Enter the second number"));
-//   alert(add(number1, number2));
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  var num1 = parseInt(prompt("Enter the first number:"));
+  var num2 = parseInt(prompt("Enter the second number"));
+  alert(add(num1, num2));
 
-  // var subtract = function(number1, number2) {
-  //   return number1 - number2;
-  // };
-  //  var num1 = parseInt(prompt("Enter number1:")); 
-  //  var num2 = parseInt(prompt("Enter num2"));
-  //  alert(subtract(num1 , num2));
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+   var num1 = parseInt(prompt("Enter number1:")); 
+   var num2 = parseInt(prompt("Enter num2"));
+   alert(subtract(num1 , num2));
 
 
-  // var multiply = function(number1, number2) {
-  //   return number1 * number2;
-  // };
-  //  var num1 = parseInt(prompt("Enter number1:")); 
-  //  var num2 = parseInt(prompt("Enter num2"));
-  //  alert(multiply(num1 , num2));
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+   var num1 = parseInt(prompt("Enter number1:")); 
+   var num2 = parseInt(prompt("Enter num2"));
+   alert(multiply(num1 , num2));
 
 
   var modulus = function(number1, number2) {
